eb32012a2f2c44a362b69b584ac855a74c6fa445
qinnnnnnnn <qinyuweifff@hotmail.com>
2015-07-24 15:11:49 +0800

    change:拆分initconfig功能

--- bin/cli.js
+++ bin/cli.js
@@ -1,12 +1,8 @@
 'use strict';
 
-
 let os = require('os');
-let path = require('path');
-let fs = require('fs-extra');
 let program = require('commander');
 let portfinder = require('portfinder');
-let cwd = process.cwd();
 
 var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
@@ -19,69 +15,64 @@ let app = Qin(config);
 let defaultPort;
 let openFile = ' ';
 
+let initConfig = require('../lib/initConfig');
 
 program
   .version(pkg.version)
-  .command('watch <filename> [otherFiles...]', '监听文件变化，自动刷新')
-  .option('-p, --port [int]', '选择服务端口', setPort)
-  .option('-o, --open <filename>', '选择默认打开文件', '')
-  .option('-i, --init', '生成配置文件', initConfig)
-  .parse(process.argv);
+  .option('-p, --port [int]', '选择服务端口', '')
+  .option('-o, --open <filename>', '选择默认打开文件', '');
 
+//生成配置文件
+program
+  .command('init')
+  .description('生成配置文件config.json')
+  .action(initConfig);
 
-//配置文件生成
-function initConfig(){
-  fs.copy(path.join(__dirname, '../test/fixtures/config.json'), cwd + '/config.json', function(err) {
-    if (err) {
-      return console.error(err);
-    } else {
-      console.log('qin: init config.json.');
-    }
-  });
-}
+program.parse(process.argv);
+
 
 
 //端口号
-function setPort(port) {
-  defaultPort = port;
-}
-
-portfinder.getPort(function(err, port) {
-  if (err) {
-    return;
-  }
-  port = defaultPort ? defaultPort : port;
-  app.listen(port,function(){
-    openURL(' http://' + getIPAddress() + ':' + port + '/' + openFile);
-  });
-});
-
-var openURL = function(url) {
-  switch (process.platform) {
-    case 'darwin':
-      exec('open' + url);
-      console.log(url);
-      break;
-    case 'win32':
-      exec('start ' + url);
-      break;
-    default:
-      spawn('xdg-open', [url]);
-  }
-  return;
-};
-
-var getIPAddress = function() {
-  var ifaces = os.networkInterfaces();
-  var ip = '';
-  for (var dev in ifaces) {
-    ifaces[dev].forEach(hanlderIface);
-  }
-  function hanlderIface(details) {
-    if (ip === '' && details.family === 'IPv4' && !details.internal) {
-      ip = details.address;
-      return;
-    }
-  }
-  return ip || '127.0.0.1';
-};
+// function setPort(port) {
+//   defaultPort = port;
+// }
+//
+// var openURL = function(url) {
+//   switch (process.platform) {
+//     case 'darwin':
+//       exec('open' + url);
+//       console.log(url);
+//       break;
+//     case 'win32':
+//       exec('start ' + url);
+//       break;
+//     default:
+//       spawn('xdg-open', [url]);
+//   }
+//   return;
+// };
+//
+// var getIPAddress = function() {
+//   var ifaces = os.networkInterfaces();
+//   var ip = '';
+//   for (var dev in ifaces) {
+//     ifaces[dev].forEach(hanlderIface);
+//   }
+//   function hanlderIface(details) {
+//     if (ip === '' && details.family === 'IPv4' && !details.internal) {
+//       ip = details.address;
+//       return;
+//     }
+//   }
+//   return ip || '127.0.0.1';
+// };
+//
+// portfinder.getPort(function(err, port) {
+//   if (err) {
+//     return;
+//   }
+//   port = defaultPort ? defaultPort : port;
+//   app.listen(port, function(){
+//     openURL(' http://' + getIPAddress() + ':' + port + '/' + openFile);
+//   });
+// });
